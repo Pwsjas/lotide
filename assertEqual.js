c856e18e8afcd74e9df15219f95ea37d22baef4e
Philip Simpson <pwsjas433@hotmail.com>
2022-06-27 18:34:13 +0000

    quick update

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('hello','hello');
-assertEqual(0, 55);
