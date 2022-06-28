e94d388ec2bbff0fc0f9c437d38bd06ac6a4d6af
Philip Simpson <pwsjas433@hotmail.com>
2022-06-28 18:41:05 +0000

    converts an array of arrays into one single array of values

---
+++ flatten.js
@@ -0,0 +1,13 @@
+const flatten = function(arr) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        output.push(arr[i][j]);
+      }
+    } else {
+      output.push(arr[i]);
+    }
+  }
+  return output;
+};
