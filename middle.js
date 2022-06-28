7d8ab9b1c27771bd0ff7b92747a1e175f85ce4f2
Philip Simpson <pwsjas433@hotmail.com>
2022-06-28 22:44:18 +0000

    return the middle value(s) of an array

---
+++ middle.js
@@ -0,0 +1,13 @@
+const middle = function(arr) {
+  let output = [];
+  if (arr.length <= 2) {
+    return output;
+  }
+  const mid = Math.floor((arr.length - 1) / 2);
+  if (arr.length % 2 === 0) {
+    output.push(arr[mid], arr[mid + 1]);
+  } else {
+    output.push(arr[mid]);
+  }
+  return output;
+};
