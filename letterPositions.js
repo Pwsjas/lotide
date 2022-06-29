4dcd1341d1e999613d8d1756d99d419cd93e1026
Philip Simpson <pwsjas433@hotmail.com>
2022-06-29 02:47:59 +0000

    added code to ignore spaces

--- letterPositions.js
+++ letterPositions.js
@@ -1,10 +1,12 @@
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
-      results[sentence[i]].push(i);
-    } else {
-      results[sentence[i]] = [i];
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
     }
   }
   return results;
