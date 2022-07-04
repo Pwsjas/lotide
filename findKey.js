dd0b975b9ff8cc7073311b77755472e8b31cd390
Philip Simpson <105725679+Pwsjas@users.noreply.github.com>
2022-07-04 15:11:34 -0400

    Update findKey.js

--- findKey.js
+++ findKey.js
@@ -1,7 +1,5 @@
 const findKey = (obj, callback) => {
   for (let key of Object.keys(obj)) {
-    console.log(key);
-    console.log(obj[key]);
     if (callback(obj[key])) {
       return key;
     }
