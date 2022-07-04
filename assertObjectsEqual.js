50f3dfd50ad24d5b5577398e1e3f8adf547ac22c
Philip Simpson <pwsjas433@hotmail.com>
2022-07-04 18:16:43 +0000

    imported functions

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,33 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } else {
-    for (let key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
-      }
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
+
 const assertObjectsEqual = (actual, expected) => {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
